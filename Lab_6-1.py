fd05d94a2f0ab1b65850c0a6d1b130c81f582d34
p23pylons <p23plyons@fairfieldprep.org>
2022-11-03 10:53:53 -0400

    Submission of Cycle 6 Day 1 Lab

---
+++ Lab_6-1.py
@@ -0,0 +1,17 @@
+# Author: PCL 10/28/22
+
+#creating variable with an array containting #'s 1-5
+my_arry = [1,2,3,4,5]
+
+#printing the #3 from my array with an index of 2
+print(my_arry[2])
+
+#printing the length of my array - should result 5
+print(len(my_arry))
+
+#printing my array repeated 3 times
+print(my_arry*3)
+
+#creating a variable for converting the str "string" into an array of each letter and printing it
+convert_to_array = list("string")
+print(convert_to_array)
